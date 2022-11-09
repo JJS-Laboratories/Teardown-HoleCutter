1f2482943cf21735e289651e1adef70a1e68816e
Jaja Steele <superjaja05@gmail.com>
2022-11-09 14:08:39 +0100

    Added Preview
    Added a preview made with sprite + lines
    TODO: Fix the preview toggle in the menu

--- main.lua
+++ main.lua
@@ -17,6 +17,8 @@ function init()
 	closeSnd = LoadSound("MOD/snd/close.ogg")
 	laserSnd = LoadLoop("MOD/snd/laser.ogg")
 	hitSnd = LoadLoop("MOD/snd/hit.ogg")
+
+	prev_circle = LoadSprite("MOD/preview_circle.png")
 	openMenu = false
 	inputAllowed = true
 	sliderList = {
@@ -29,6 +31,8 @@ function init()
 	autoStopToggle = true
 	autoStop = true
 	startMenu = true
+	clearAll = false
+	showPreview = true
 end
 
 --Return a random vector of desired length
@@ -38,6 +42,12 @@ function rndVec(length)
 end
 
 function makeSlider(x,y,min,max,name,format,multiplier)
+	UiPush()
+	UiTranslate(x+104,y+14)
+	UiColor(0.15,0.15,0.15,0.75)
+	UiRect(max-min,4)
+	UiPop()
+
 	UiPush()
 	UiTranslate(x+90,y)
 	sliderList[name] = UiSlider("slider.png", "x", sliderList[name], min, max)
@@ -69,12 +79,12 @@ function draw(dt)
 	if openMenu or startMenu then
 		mw = UiCenter()
 		mh = UiMiddle()
-		UiBlur(0.5)
+		UiBlur(0.05)
 		UiMakeInteractive()
 		UiPush()
 		UiTranslate(mw-150,mh-100)
 		UiColor(0.3,0.3,0.3,0.5)
-		UiRect(300,200)
+		UiRect(300,300)
 		UiPop()
 
 		height = makeSlider(mw-140,mh-90,0,100,"Height","%.1f",0.15)
@@ -90,19 +100,31 @@ function draw(dt)
 		else
 			autoStopToggle = true
 		end
+		
 		if makeTextButton(mw-140,mh+110,10,60,"Default",nil) then
 			openMenu = false
 			sliderList = {
 				Height=33.33,
 				Depth=8,
-				Radius=75,
+				Radius=30,
 				Speed=15
 			}
 			autoStop = true
 			startMenu = true
 		end
+		
 		if startMenu then
 			startMenu = false
+		end
+		clearAll = makeTextButton(mw-140,mh+140,10,60,"Clear All",nil)
+
+		if makeTextButton(mw-140,mh+170,10,60,"Preview",tostring(showPreview)) then
+			if showPreviewToggle then
+				showPreview = not showPreview
+				showPreviewToggle = false
+			end
+		else
+			showPreviewToggle = true
 		end
 	end
 end
@@ -120,6 +142,66 @@ function tick(dt)
 			inputAllowed = true
 		end
 
+		--Draws the Preview Lines
+		local camera_transform = GetCameraTransform()
+		local camera_forward = TransformToParentVec(camera_transform, Vec(0, 0, -1))
+		local prev_hit, prev_dist, prev_normal, prev_shape = QueryRaycast(camera_transform.pos, camera_forward, 5)
+
+		local prev_rot = QuatLookAt({0,0,0},prev_normal)
+		local prev_quat = QuatEuler(-90, 0, 0)
+		local prev_rot_final = QuatRotateQuat(prev_rot,prev_quat)
+
+		if prev_hit then
+			local prev_hitpoint = VecAdd(camera_transform.pos, VecScale(camera_forward, prev_dist))
+			local prev_hitpoint_zfix = VecAdd(prev_hitpoint, QuatRotateVec(prev_rot_final,{0,0.01,0}))
+
+			DrawSprite(prev_circle, Transform(prev_hitpoint_zfix,QuatRotateQuat(prev_rot_final,QuatEuler(90,0,0))), (radius/10-0.1)*2, (radius/10-0.1)*2,0,1,0.1,0.1,true,true)
+			DrawSprite(prev_circle, Transform(prev_hitpoint,QuatRotateQuat(prev_rot_final,QuatEuler(90,0,0))), (radius/10-0.1)*2, (radius/10-0.1)*2,0,1,0.1,0.01,false,true)
+
+			prev_point_up = VecAdd(prev_hitpoint,QuatRotateVec(prev_rot_final,{0,0,(radius/10-0.1)}))
+			prev_point_down = VecAdd(prev_hitpoint,QuatRotateVec(prev_rot_final,{0,0,(radius/10-0.1)*-1}))
+			prev_point_right = VecAdd(prev_hitpoint,QuatRotateVec(prev_rot_final,{(radius/10-0.1),0,0}))
+			prev_point_left = VecAdd(prev_hitpoint,QuatRotateVec(prev_rot_final,{(radius/10-0.1)*-1,0,0}))
+
+			prev_point_height_up = VecAdd(prev_point_up,QuatRotateVec(prev_rot_final,{0,height/10,0}))
+			prev_point_height_down = VecAdd(prev_point_down,QuatRotateVec(prev_rot_final,{0,height/10,0}))
+			prev_point_height_right = VecAdd(prev_point_right,QuatRotateVec(prev_rot_final,{0,height/10,0}))
+			prev_point_height_left = VecAdd(prev_point_left,QuatRotateVec(prev_rot_final,{0,height/10,0}))
+
+			prev_point_depth_up = VecAdd(prev_point_height_up,QuatRotateVec(prev_rot_final,{0,-(depth/10),0}))
+			prev_point_depth_down = VecAdd(prev_point_height_down,QuatRotateVec(prev_rot_final,{0,-(depth/10),0}))
+			prev_point_depth_right = VecAdd(prev_point_height_right,QuatRotateVec(prev_rot_final,{0,-(depth/10),0}))
+			prev_point_depth_left = VecAdd(prev_point_height_left,QuatRotateVec(prev_rot_final,{0,-(depth/10),0}))
+
+			DrawLine(prev_hitpoint,prev_point_up,1,0,0) --Up
+			DebugLine(prev_hitpoint,prev_point_up,1,0,0,0.35) --Up (transparent)
+
+			DrawLine(prev_point_height_up,prev_point_depth_up,1,0,0) --Up (depth+height)
+			DebugLine(prev_point_height_up,prev_point_depth_up,1,0,0,0.35) --Up (depth+height, transparent)
+
+
+			DrawLine(prev_hitpoint,prev_point_down,1,0,0) --Down
+			DebugLine(prev_hitpoint,prev_point_down,1,0,0,0.35) --Down (transparent)
+
+			DrawLine(prev_point_height_down,prev_point_depth_down,1,0,0) --Down (depth+height)
+			DebugLine(prev_point_height_down,prev_point_depth_down,1,0,0,0.35) --Down (depth+height, transparent)
+
+
+			DrawLine(prev_hitpoint,prev_point_right,1,0,0.2) --Right
+			DebugLine(prev_hitpoint,prev_point_right,1,0,0.2,0.35) --Right (transparent)
+
+			DrawLine(prev_point_height_right,prev_point_depth_right,1,0,1) --Right (depth+height)
+			DebugLine(prev_point_height_right,prev_point_depth_right,1,0,1,0.35) --Right (depth+height, transparent)
+
+
+			DrawLine(prev_hitpoint,prev_point_left,1,0,0.2) --Left
+			DebugLine(prev_hitpoint,prev_point_left,1,0,0.2,0.35) --Left (transparent)
+
+			DrawLine(prev_point_height_left,prev_point_depth_left,1,0,0.2) --Left (depth+height)
+			DebugLine(prev_point_height_left,prev_point_depth_left,1,0,0.2,0.35) --Left (depth+height, transparent)
+
+		end
+
 		--Check if tool is firing
 		if GetBool("game.player.canusetool") and InputDown("usetool") and GetFloat("game.tool.supercutter.ammo") > 0 and #FindShapes("cutterboxVox",true) == 0 then
 			if enableFire then
@@ -129,7 +211,7 @@ function tick(dt)
 				PlayLoop(laserSnd)
 				local t = GetCameraTransform()
 				local fwd = TransformToParentVec(t, Vec(0, 0, -1))
-				local maxDist = 3
+				local maxDist = 5
 				local hit, dist, normal, shape = QueryRaycast(t.pos, fwd, maxDist)
 				if not hit then
 					dist = maxDist
@@ -187,7 +269,7 @@ function tick(dt)
 				RemoveTag(shape,"isNew")
 				SetTag(shape,"isCutting")
 				SetTag(shape,"lifetime","0")
-			elseif (tonumber(GetTagValue(shape, "lifetime")) > 120*(3/speed) and autoStop and GetJointMovement(cutterJoint) > 0) or (GetBool("game.player.canusetool") and InputDown("grab") and not autoStop) then
+			elseif (tonumber(GetTagValue(shape, "lifetime")) > 120*(3/speed) and autoStop and GetJointMovement(cutterJoint) > 0) or (GetString("game.player.tool") == "supercutter" and InputDown("grab") and not autoStop) or clearAll then
 			  	Delete(cutterJoint)
 			  	Delete(shape)
 			  	RemoveTag(shape,"isCutting")
@@ -218,6 +300,8 @@ function tick(dt)
 				local cuttingpoint = TransformToParentPoint(shapeTransform,Vec(0.2,radius/10, height/10))
 				local cuttingrot = TransformToParentVec(shapeTransform, Vec(0, 0, -1))
 
+				local fixed_shape_pos = VecAdd(shapeTransform.pos,QuatRotateVec(shapeTransform.rot,{0.2,0.2,0.1}))
+
 				-- DebugLine(shapeTransform.pos, TransformToParentPoint(shapeTransform, Vec(0,0,-1)), 0, 0, 1)
 				-- DebugLine(shapeTransform.pos, TransformToParentPoint(shapeTransform, Vec(0,1,0)), 0, 1, 0)
 
@@ -228,7 +312,10 @@ function tick(dt)
 				
 				local hitpoint = VecAdd(cuttingpoint, VecScale(cuttingrot, dist+0.1))
 
+				DrawLine(fixed_shape_pos,cuttingpoint,1,0,0.3)
+
 				if hit then
+					DrawLine(cuttingpoint,hitpoint,1,0.2,0.2)
 					-- DebugPrint("Hit!")
 					-- DebugPrint(dist)
 					-- DebugCross(hitpoint)
